32b90a50ad13974ca1ca49cd1d9fbae44c006ed9
admin <admin>
2018-08-01 14:51:50 +0800

    some change

--- app/src/main/java/com/bt/andy/zhedian/fragment/TotalGoodsFragment.java
+++ app/src/main/java/com/bt/andy/zhedian/fragment/TotalGoodsFragment.java
@@ -232,10 +232,10 @@ public class TotalGoodsFragment extends Fragment implements View.OnClickListener
         if (local == 0) {
             ToastUtils.showToast(getContext(), "未在本项目中查找到该流转卡号");
         }
-        local = local + 6;
+        local = local + 7;
         mRecData.set(local, "请点击修改");
         adapter.notifyDataSetChanged();
-        rec_detail.scrollToPosition(local + 6);
+        rec_detail.scrollToPosition(local + 7);
     }
 
     private void scanningCode(int kind) {
